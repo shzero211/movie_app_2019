c19b61174090181926991da4acfc355117550326
SangHun <shzero211@naver.com>
2020-12-27 17:45:00 +0900

    "(6) setstate"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from "prop-types"
-//클래스 컴포넌트는 function 컴포너트와 달리 return 을 할수없으며 render 을 통해 return 해서 결과 출력 가능
+
 class App extends React.Component{
   state={
     count:0
   };
-  add=()=>{console.log("add")}
-  minus=()=>{console.log("minus")}
+  //setstate를 호출할때마다 react는 새로운 state 와 함께 render function울 호출할것이다.
+  add=()=>{this.setState(current=>({count:current.count+1}))};
+
+  minus=()=>{this.setState(current=>({count:current.count-1}))};
   render() {
    return (<div>
      <h1>the number is:{this.state.count}</h1>
