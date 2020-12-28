a72b20eec4552d9f21352c20aba2a620b0d93a62
SangHun <shzero211@naver.com>
2020-12-28 13:36:30 +0900

    "(7) use component life cycle"

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import React from 'react';
 import PropTypes from "prop-types"
 
 class App extends React.Component{
-  state={
-    count:0
-  };
-  //setstate를 호출할때마다 react는 새로운 state 와 함께 render function울 호출할것이다.
-  add=()=>{this.setState(current=>({count:current.count+1}))};
-
-  minus=()=>{this.setState(current=>({count:current.count-1}))};
-  render() {
-   return (<div>
-     <h1>the number is:{this.state.count}</h1>
-    <button onClick={this.add}>Add</button>
-    <button onClick={this.minus}>Minus</button></div>
-   );
-  }
+state={
+  isLoading:true
+};
+componentDidMount(){
+  setTimeout(()=>this.setState({isLoading:false}),6000)
+}
+render(){
+  const {isLoading}=this.state;
+  return <div>{isLoading?"Loading...":"we are ready"}</div>;
+}
 }
 export default App;
