2357ff262bee06b4bafecd94a5049aa71559735c
SangHun <shzero211@naver.com>
2020-12-28 17:27:19 +0900

    "(11) adding Genres"

--- src/App.js
+++ src/App.js
@@ -3,34 +3,34 @@ import axios from 'axios';
 import Movie from './Movie'
 import "./App.css"
 
-import PropTypes from "prop-types"
-//state를 이용하기위해서 class component를 사용합니다
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: []
   };
-  //axios는 npm i axios 를 통해 다운받았고 url에서 정보를 받을때쓴다. async 와 wait 는 정보가져오는데 시간이 걸린다는 것을 나타냄
   getMovies = async () => {
-    //ES6에서의 기능으로 오브젝트 안에 있는 값을 불러올 수 있습니다.
+    
     const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
     this.setState({ movies, isLoading: false });
   };
+
   componentDidMount() {
     this.getMovies();
   }
+  
   render() {
-    //this.state를 생략하기위한 const
+   
     const { isLoading, movies } = this.state;
-    //object를 풀어줄 때 map함수를 사용하고, 또, jsx에서는 props를 통해서 값을 전달합니다. key는 표현되지 않지만 필수props입니다.
+    
     return( 
-    <section class="container">
+      //자바스크립트안에서 html class는 className으로 작성해야함
+    <section className="container">
       {isLoading ? (
-      <div class="loader">
-        <span class="loader_text">Loading....</span>
+      <div className="loader">
+        <span className="loader_text">Loading....</span>
       </div> 
       ): (
-        <div class="movies">
+        <div className="movies">
         {
           movies.map(movie=>(
             <Movie
@@ -40,6 +40,7 @@ class App extends React.Component {
             title={movie.title}
             summary={movie.summary}
             poster={movie.medium_cover_image}
+            genres={movie.genres}
             />
           ) )}
 
--- src/Movie.js
+++ src/Movie.js
@@ -1,15 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css"
-function Movie({year, title, summary, poster }) {
-    return <div class="movie">
-        <img src={poster} alt={title} title={title}/>
-        <div class="movie_data">
-            <h3 class="movie_title">{title}</h3>
-            <h5 class="movie_year">{year}</h5>
-            <p class="movie_summary" P>{summary}</p>
+function Movie({ year, title, summary, poster, genres }) {
+    return (
+        <div className="movie">
+            <img src={poster} alt={title} title={title} />
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="genres">
+                    {genres.map((genre, index) => (
+                        <li key={index} className="genres__genre">
+                            {genre}
+                        </li>
+                    ))}
+                </ul>
+                <p className="movie__summary">{summary}</p>
+            </div>
         </div>
-    </div>;
+    );
 }
 //propsTypes는 number, string, bool, func, node등 다양하게 존재합니다.
 Movie.propTypes = {
@@ -17,7 +26,8 @@ Movie.propTypes = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.number.isRequired
+    poster: PropTypes.number.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 //export해줘야 다른 js에서 Movie라는 component를 사용할 수 있습니다.
 export default Movie;
