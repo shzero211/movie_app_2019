47411930253213b14140c70fc950646c9fe23f02
SangHun <shzero211@naver.com>
2020-12-31 00:55:10 +0900

    "sharing props between routes"

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,23 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css"
 function Movie({ year, title, summary, poster, genres }) {
     return (
+        <Link
+        to={{
+          pathname: "/movie-detail",
+          state: {
+            year,
+            title,
+            summary,
+            poster,
+            genres
+          }
+        }}
+      >
         <div className="movie">
-            <img src={poster} alt={title} title={title} />
-            <div className="movie__data">
-                <h3 className="movie__title">{title}</h3>
-                <h5 className="movie__year">{year}</h5>
-                <ul className="movie__genres">
-                    {genres.map((genre, index) => (
-                        <li key={index} className="genres__genre">
-                            {genre}
-                        </li>
-                    ))}
-                </ul>
-                <p className="movie__summary">{summary.slice(0,180)}...</p>
-            </div>
-        </div>
+          <img src={poster} alt={title} title={title} />
+          <div className="movie__data">
+            <h3 className="movie__title">{title}</h3>
+            <h5 className="movie__year">{year}</h5>
+            <ul className="movie__genres">
+              {genres.map((genre, index) => (
+                <li key={index} className="genres__genre">
+                  {genre}
+                </li>
+              ))}
+            </ul>
+            <p className="movie__summary">{summary.slice(0, 180)}...</p>
+          </div>
+          </div>
+          </Link>
     );
 }
 //propsTypes는 number, string, bool, func, node등 다양하게 존재합니다.
